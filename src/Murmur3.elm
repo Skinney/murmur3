16e8c7f22153169d5d37955570b15ad4098a951d
Matthew Griffith <mdg.griffith@gmail.com>
2016-11-15 08:39:10 -0500

    update to 0.18

--- src/Murmur3.elm
+++ src/Murmur3.elm
@@ -27,7 +27,10 @@ hashFold : Char -> ( Int, Int, Int ) -> ( Int, Int, Int )
 hashFold c ( shift, seed, hash ) =
     let
         res =
-            (((Char.toCode c) `and` 0xFF) `shiftLeft` shift) `or` hash
+            0xFF
+                |> and (Char.toCode c)
+                |> shiftLeftBy shift
+                |> (flip or) hash
     in
         if shift >= 24 then
             let
@@ -49,22 +52,20 @@ finalize strLength ( _, seed, hash ) =
                 seed
 
         h1 =
-            acc `Bitwise.xor` strLength
+            Bitwise.xor acc strLength
 
         h2 =
-            h1
-                `Bitwise.xor` (h1 `shiftRightLogical` 16)
+            Bitwise.xor h1 (shiftRightZfBy 16 h1)
                 |> mur 0x85EBCA6B
 
         h3 =
-            h2
-                `Bitwise.xor` (h2 `shiftRightLogical` 13)
+            Bitwise.xor h2 (shiftRightZfBy 13 h2)
                 |> mur 0xC2B2AE35
 
         h4 =
-            h3 `Bitwise.xor` (h3 `shiftRightLogical` 16)
+            Bitwise.xor h3 (shiftRightZfBy 16 h3)
     in
-        h4 `shiftRightLogical` 0
+        shiftRightZfBy 0 h4
 
 
 mix : Int -> Int -> Int
@@ -74,24 +75,37 @@ mix h1 h2 =
             mur 0xCC9E2D51 h2
 
         k2 =
-            (k1 `shiftLeft` 15)
-                `or` (k1 `shiftRightLogical` 17)
+            or (shiftLeftBy 15 k1) (shiftRightZfBy 17 k1)
                 |> mur 0x1B873593
     in
-        h1 `Bitwise.xor` k2
+        Bitwise.xor h1 k2
 
 
 mur : Int -> Int -> Int
 mur c h =
-    (((h `and` 0xFFFF) * c) + ((((h `shiftRightLogical` 16) * c) `and` 0xFFFF) `shiftLeft` 16)) `and` 0xFFFFFFFF
+    0xFFFF
+        |> and h
+        |> (*) c
+        |> (+)
+            (h
+                |> shiftRightZfBy 16
+                |> (*) c
+                |> and 0xFFFF
+                |> shiftLeftBy 16
+            )
+        |> and 0xFFFFFFFF
 
 
 step : Int -> Int
 step acc =
     let
         h1 =
-            (acc `shiftLeft` 13)
-                `or` (acc `shiftRightLogical` 19)
+            or (shiftLeftBy 13 acc) (shiftRightZfBy 19 acc)
                 |> mur 5
     in
-        ((h1 `and` 0xFFFF) + 0x6B64) + ((((h1 `shiftRightLogical` 16) + 0xE654) `and` 0xFFFF) `shiftLeft` 16)
+        h1
+            |> shiftRightZfBy 16
+            |> (+) 0xE654
+            |> and 0xFFFF
+            |> shiftLeftBy 16
+            |> (+) ((and h1 0xFFFF) + 0x6B64)
